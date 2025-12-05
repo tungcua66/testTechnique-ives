635b43b168c31597a3caec0472e54e091d3c9d24
tungcua66 <tunghuong66bg@gmail.com>
2025-12-05 14:44:35 +0100

    chore(app): add urlencoded parser for oauth token

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import taskRoutes from "./routes/taskRoutes";
+import taskRoutes from "./routes/taskRoutes.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/tasks", taskRoutes);
 
